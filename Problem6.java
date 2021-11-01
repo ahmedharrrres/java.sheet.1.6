5b9660ded694a5f43d6b50627f1504c1688b428c
ahmedharrrres <92724993+ahmedharrrres@users.noreply.github.com>
2021-11-02 01:20:54 +0200

    Add files via upload

---
+++ Problem6.java
@@ -0,0 +1,15 @@
+package problem6;
+
+public class Problem6 {
+	public static void main(String[]args) {
+		System.out.println("********");
+		System.out.println(" ********");
+		System.out.println("********");
+		System.out.println(" ********");
+		System.out.println("********");
+		System.out.println(" ********");
+		System.out.println("********");
+		System.out.println(" ********");
+
+	}
+}
